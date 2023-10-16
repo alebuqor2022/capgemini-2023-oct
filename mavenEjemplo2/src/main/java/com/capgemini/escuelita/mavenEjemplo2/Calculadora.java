a11e465b7e21dac082a6a08a425b19fe1182edd4
alebuqor2022 <97527361+alebuqor2022@users.noreply.github.com>
2023-10-16 05:23:57 -0300

    Add files via upload

---
+++ mavenEjemplo2/src/main/java/com/capgemini/escuelita/mavenEjemplo2/Calculadora.java
@@ -0,0 +1,14 @@
+package com.capgemini.escuelita.mavenEjemplo2;
+
+public class Calculadora {
+
+	public int sumar(int val1, int val2) {
+		System.out.println("estoy sumando");
+		return val1+val2;
+	}
+	
+	public int restar(int val1, int val2) {
+		System.out.println("estoy restando");
+		return val1-val2;
+	}
+}
