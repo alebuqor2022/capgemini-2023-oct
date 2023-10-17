29119d48e822b39c8cfe12d8e1a32324e3177fa6
alebuqor2022 <97527361+alebuqor2022@users.noreply.github.com>
2023-10-17 11:50:36 -0300

    Add files via upload

---
+++ ScriptsSql/INSERT_managers.sql
@@ -0,0 +1,7 @@
+INSERT INTO `capgemini_asturias`.`managers` (`manager_id`, `manager_name`, `role`) VALUES ('100', 'VP', 'admin');
+INSERT INTO `capgemini_asturias`.`managers` (`manager_id`, `manager_name`, `role`) VALUES ('103', 'Gerente', 'admin');
+INSERT INTO `capgemini_asturias`.`managers` (`manager_id`, `manager_name`, `role`) VALUES ('124', 'Presidente', 'admin');
+INSERT INTO `capgemini_asturias`.`managers` (`manager_id`, `manager_name`, `role`) VALUES ('149', 'Director General', 'admin');
+INSERT INTO `capgemini_asturias`.`managers` (`manager_id`, `manager_name`, `role`) VALUES ('200', 'Director Finanzas', 'admin');
+INSERT INTO `capgemini_asturias`.`managers` (`manager_id`, `manager_name`, `role`) VALUES ('201', 'Director Ventas', 'admin');
+INSERT INTO `capgemini_asturias`.`managers` (`manager_id`, `manager_name`, `role`) VALUES ('205', 'Director RRHH', 'admin');
