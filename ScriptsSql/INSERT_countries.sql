6adc462a15a775ddae8c01b8dd3baa27eb222aa1
alebuqor2022 <97527361+alebuqor2022@users.noreply.github.com>
2023-10-17 11:39:35 -0300

    Add files via upload

---
+++ ScriptsSql/INSERT_countries.sql
@@ -0,0 +1,4 @@
+INSERT INTO `capgemini_asturias`.`countries` (`country_id`, `country_name`, `region_id`) VALUES ('1', 'España', '1');
+INSERT INTO `capgemini_asturias`.`countries` (`country_id`, `country_name`, `region_id`) VALUES ('2', 'Argentina', '8');
+INSERT INTO `capgemini_asturias`.`countries` (`country_id`, `country_name`, `region_id`) VALUES ('3', 'USA', '9');
+INSERT INTO `capgemini_asturias`.`countries` (`country_id`, `country_name`, `region_id`) VALUES ('4', 'Gales', '10');
