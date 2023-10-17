0fec3a95d129275644f1a6493ba848268102197e
alebuqor2022 <97527361+alebuqor2022@users.noreply.github.com>
2023-10-17 12:01:48 -0300

    Add files via upload

---
+++ ScriptsSql/INSERT_jobs.sql
@@ -0,0 +1,5 @@
+INSERT INTO `capgemini_asturias`.`jobs` (`job_id`, `job_title`, `min_salary`, `max_salary`) VALUES ('1', 'it_prog', '2600', '4000');
+INSERT INTO `capgemini_asturias`.`jobs` (`job_id`, `job_title`, `min_salary`, `max_salary`) VALUES ('2', 'st_man', '1400', '2000');
+INSERT INTO `capgemini_asturias`.`jobs` (`job_id`, `job_title`, `min_salary`, `max_salary`) VALUES ('3', 'pr_rep', '1100', '1800');
+INSERT INTO `capgemini_asturias`.`jobs` (`job_id`, `job_title`, `min_salary`, `max_salary`) VALUES ('4', 'SA_REP', '1500', '2200');
+INSERT INTO `capgemini_asturias`.`jobs` (`job_id`, `job_title`, `min_salary`, `max_salary`) VALUES ('5', 'AD_VP', '1700', '2500');
