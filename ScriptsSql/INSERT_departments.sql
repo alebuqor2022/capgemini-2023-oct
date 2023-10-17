90635bbf4eb9dc7a0305da8e0ccfe2eb20de9db6
alebuqor2022 <97527361+alebuqor2022@users.noreply.github.com>
2023-10-17 11:37:24 -0300

    Add files via upload

---
+++ ScriptsSql/INSERT_departments.sql
@@ -0,0 +1,16 @@
+INSERT INTO departments (department_id, department_name, manager_id, location_id)
+Values(10,'Administration',200,1700);
+INSERT INTO departments (department_id, department_name, manager_id, location_id)
+Values(20,'Marketing',201,1800);
+INSERT INTO departments (department_id, department_name, manager_id, location_id)
+Values(50,'Shipping',124,1500);
+INSERT INTO departments (department_id, department_name, manager_id, location_id)
+Values(60,'IT',103,1400);
+INSERT INTO departments (department_id, department_name, manager_id, location_id)
+Values(80,'Sales',149,2500);
+INSERT INTO departments (department_id, department_name, manager_id, location_id)
+Values(90,'Executive',100,1700);
+INSERT INTO departments (department_id, department_name, manager_id, location_id)
+Values(110,'Accounting',205,1700);
+INSERT INTO departments (department_id, department_name, manager_id, location_id)
+Values(190,'Contracting',null,1700);
