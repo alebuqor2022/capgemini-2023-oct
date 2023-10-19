0471e135d881de7880abd3fe26008447bfb6955e
alebuqor2022 <97527361+alebuqor2022@users.noreply.github.com>
2023-10-19 07:47:03 -0300

    Add files via upload

--- DemoJPA/src/main/java/dao/DaoPersona.java
+++ DemoJPA/src/main/java/dao/DaoPersona.java
@@ -1,6 +1,7 @@
 package dao;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
 
 import model.Persona;
 import utils.JpaUtils;
@@ -21,5 +22,52 @@ public static void create(Persona p) {
 		em.close();
 	}
 	
+}
+
+public static Persona find(Long id) {
+	EntityManager em=JpaUtils.getEmf().createEntityManager();
+	Persona p=null;
+	try {
+	p=em.find(Persona.class, id);
+	}catch (Exception e) {
+		System.out.println("no se encontro" + e.getMessage());
+	}finally {
+		em.close();
+	}
+	return p;
+}
+
+public static void update(Persona p) {
+	EntityManager em=JpaUtils.getEmf().createEntityManager();
+	EntityTransaction tx=em.getTransaction();
+	tx.begin();
+	
+	try {
+		em.merge(p); // hace el update
+		tx.commit();
+		System.out.println("exito en la actualizacion");
+	} catch (Exception e) {
+		tx.rollback(); // retroceso, no hace cambios
+		System.out.println("no se pudo modificar " + e.getMessage());
+	} finally {
+		em.close();
+	}
+	
+	
+}
+
+public static void delete(Long id) {
+	EntityManager em=JpaUtils.getEmf().createEntityManager();
+	em.getTransaction().begin();
+	try {
+		Persona p=em.find(Persona.class, id);
+		em.remove(p); // Delete * from personas where personas.id= id
+		em.getTransaction().commit();
+	}catch (Exception e) {
+		em.getTransaction().rollback(); // retroceso, no hace cambios
+		System.out.println(e.getMessage());
+	} finally {
+		em.close();
+	}
 }
 }
--- DemoJPA/src/main/java/ejecutable/Inicio.java
+++ DemoJPA/src/main/java/ejecutable/Inicio.java
@@ -9,17 +9,38 @@ import model.Persona;
 public class Inicio {
 
 	public static void main(String[] args) {
-		crearPersona();
+		//crearPersona();
+		buscarPersona(2L);
+		//actualizarPersona() ;
+		//borraPersona(1L);
 
 	}
 	
 	public static void crearPersona() {
 		Persona p=new Persona();
-		p.setNombre("Ale");
-		p.setApellido("Buq");
+		p.setNombre("Jose");
+		p.setApellido("Garcia");
 		LocalDate fecha=LocalDate.of(1990,01,07);
 		p.setFechaNacimiento(fecha);
 		DaoPersona.create(p);
 	}
+	
+	public static void buscarPersona(Long id) {
+		Persona p=DaoPersona.find(id);
+		System.out.println("la persona se llama " + p.getNombre());
+	}
 
+	public static void actualizarPersona() {
+		Persona p=DaoPersona.find(3L);
+		p.setApellido("Trefila");
+		p.setNombre("Federico");
+		
+		DaoPersona.update(p);
+		buscarPersona(3L);
+	}
+	
+	public static void borraPersona(Long id) {
+		DaoPersona.delete(id);
+		
+	}
 }
