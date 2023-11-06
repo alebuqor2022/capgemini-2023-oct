8bc40dff92c2657c3c6e8f1fd6ac8731c6c21570
alebuqor2022 <97527361+alebuqor2022@users.noreply.github.com>
2023-11-06 15:34:41 -0300

    Delete ProyectosReact directory

--- ProyectosReact/App.js
+++
@@ -1,8 +0,0 @@
-import { createAppContainer } from "react-navigation";
-import { createStackNavigator } from "react-navigation-stack";
-import Home from "./Home";
-import Details from "./Details";
-
-
-export default createAppContainer(
-  createStackNavigator( {Home, Details},{initialRouteName: "Home"}));
--- ProyectosReact/readme.txt
+++
@@ -1,1 +0,0 @@
-No se subio la carpeta de node_modules
